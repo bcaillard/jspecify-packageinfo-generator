4fe555c7385d34387b71185e81540529a1bb912d
Benjamin CAILLARD <benjamin.caillard.dev@gmail.com>
2025-07-12 11:26:07 +0200

    ci: Hello World CI.

---
+++ src/main/java/dev/bencai/jspecifyutilities/packageinfogenerator/HelloWorld.java
@@ -0,0 +1,9 @@
+package dev.bencai.jspecifyutilities.packageinfogenerator;
+
+public class HelloWorld {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
