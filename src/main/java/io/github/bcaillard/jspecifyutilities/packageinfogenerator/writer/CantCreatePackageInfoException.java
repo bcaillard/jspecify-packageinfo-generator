43ea7d1c439f45a5e8bd025a6cda6febaef4b72a
Benjamin CAILLARD <benjamin.caillard.dev@gmail.com>
2025-07-14 09:30:42 +0200

    test publish

--- src/main/java/io/github/bcaillard/jspecifyutilities/packageinfogenerator/writer/CantCreatePackageInfoException.java
+++ src/main/java/io/github/bcaillard/jspecifyutilities/packageinfogenerator/writer/CantCreatePackageInfoException.java
@@ -21,9 +21,8 @@ import java.io.IOException;
 
 /**
  * Exception thrown when the creation of a {@code package-info.java} file fails.
- * <p>
- * This exception is used to wrap {@link IOException} instances that occur during the process of generating or writing a {@code package-info.java} file, providing additional context about the
- * failure.<p>
+ * <p>This exception is used to wrap {@link IOException} instances that occur during the process of generating or writing a {@code package-info.java} file, providing additional context about the
+ * failure.</p>
  */
 public class CantCreatePackageInfoException extends RuntimeException {
 
